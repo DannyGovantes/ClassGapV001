4061c80334814b389223a96815eaae7cd915f506
Danny Govantes <32424801+DannyGovantes@users.noreply.github.com>
2019-10-26 12:30:35 -0500

    Original

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -2,19 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class MainMenu :Menu<MainMenu> {
-    public Vector3 CameraPos;
-
-    public Vector3 desiredPos;
-
-
-    private void LateUpdate() {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Vector3 pos = CameraPos;
-            Vector3 desired = Vector3.Lerp(pos,desiredPos, 0.12f);
-            desiredPos = desired;
-            Camera.main.transform.position = desiredPos;
-        }
-    }
+   }
 
     
 }
